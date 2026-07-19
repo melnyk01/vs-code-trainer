3a69b2ade90c892e4bb3af47ceb58297164606e3
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-07-19 12:34:46 +0200

    Refractor code, add random shortcut generator

--- javascript.js
+++ javascript.js
@@ -1,65 +1,29 @@
-// {
-//     id: ,
-//     action: ,
-//     shortcut: ,
-//     keys: {},
-const shortcutList = [
-    {
-        id: 1,
-        action: "Copy current line",
-        display: "Ctrl+C",
-        keys: {
-            ctrl: true,
-            key: "c",
-        }
-    },
-    {
-        id: 2,
-        action: "Paste copied line",
-        display: "Ctrl+V",
-        keys: {
-            ctrl: true,
-            key: "v",
-        }
-    },
-    {
-        id: 3,
-        action: "Select variable",
-        display: "Ctrl+D",
-        keys: {
-            ctrl: true,
-            key: "d"
-        }
-    },
-    {
-        id: 4,
-        action: "Save file",
-        display: "Ctrl+S",
-        keys: {
-            ctrl: true,
-            key: "s",
-        }
-    },
-]
-
-let ui =
+const ui =
 {
     mainContent: document.querySelector('.main-content'),
+    generateBtn: (function () {
+        button = document.querySelector('#generate');
+        button.addEventListener('click', () => {
+            game.generateShortcut();
+            ui.displayShortcut();
+        });
+
+    })(),
     h2: document.querySelector('#shortcut'),
     p: document.querySelector('#shortcut-instruction'),
 
-    displayShortcut(shortcut) {
-        this.h2.textContent = `Let's learn shortcut "${shortcut.action}"`
-        this.p.textContent = `Press ${shortcut.display}`
+    displayShortcut() {
+        this.h2.textContent = `Let's learn shortcut "${game.currentShortcut.action}"`
+        this.p.textContent = `Press ${game.currentShortcut.display}`
     },
 
-    listenToInput(shortcut) {
+    listenToInput() {
         let userCombination = {};
 
         document.addEventListener('keydown', (event) => {
             userCombination.ctrl = event.ctrlKey;
             userCombination.key = event.key;
-            if (checkShortcut(shortcut, userCombination)) {
+            if (game.checkShortcut(game.currentShortcut, userCombination)) {
                 console.log("Nicely done!")
             }
         }
@@ -67,21 +31,61 @@ let ui =
     },
 }
 
-function game() {
-    const shortcut = shortcutList[(Math.floor(Math.random() * shortcutList.length))];
-    ui.displayShortcut(shortcut);
-    ui.listenToInput(shortcut);
+const game = {
+    shortcutList: [
+        {
+            id: 1,
+            action: "Copy current line",
+            display: "Ctrl+C",
+            keys: {
+                ctrl: true,
+                key: "c",
+            }
+        },
+        {
+            id: 2,
+            action: "Paste copied line",
+            display: "Ctrl+V",
+            keys: {
+                ctrl: true,
+                key: "v",
+            }
+        },
+        {
+            id: 3,
+            action: "Select variable",
+            display: "Ctrl+D",
+            keys: {
+                ctrl: true,
+                key: "d"
+            }
+        },
+        {
+            id: 4,
+            action: "Save file",
+            display: "Ctrl+S",
+            keys: {
+                ctrl: true,
+                key: "s",
+            }
+        },
+    ],
+    currentShortcut: '',
+    generateShortcut() {
+        this.currentShortcut = this.shortcutList[(Math.floor(Math.random() * this.shortcutList.length))]
+    },
+    checkShortcut(shortcut, userCombination) {
+        return (
+            shortcut.keys.ctrl === userCombination.ctrl &&
+            shortcut.keys.key === userCombination.key
+        )
+    }
 }
 
-function checkShortcut(shortcut, userCombination) {
-    return (
-        shortcut.keys.ctrl === userCombination.ctrl &&
-        shortcut.keys.key === userCombination.key
-    )
+function play() {
+    game.generateShortcut();
+    ui.displayShortcut();
+    ui.listenToInput();
 }
 
-game();
-
-
-
-
+play();
